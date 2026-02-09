eea8adc94b958a1aff5aeb27c75135207cf77ccc
erikarika80 <erikadwiramadhani87@gmail.com>
2026-02-09 13:59:35 +1200

    fixing

--- resources/views/auth/register-admin.blade.php
+++ resources/views/auth/register-admin.blade.php
@@ -5,7 +5,7 @@
     <title>Register Admin</title>
 
     <!-- CSS -->
-    <link rel="stylesheet" href="{{ asset('css/login/auth.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
 
     <!-- Font Awesome (icon mata) -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
--- resources/views/auth/register-anggota.blade.php
+++ resources/views/auth/register-anggota.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <title>Register Anggota</title>
 
-  <link rel="stylesheet" href="{{ asset('css/login/auth.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/auth.css') }}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
